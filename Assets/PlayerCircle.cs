9e2762a77c19da1cd43efacd2d1fcb8aea8dfc25
Sachin rao <biz.sachinrao@gmail.com>
2023-10-13 20:01:10 +0530

    Added Random Color

--- Assets/PlayerCircle.cs
+++ Assets/PlayerCircle.cs
@@ -5,7 +5,20 @@ using UnityEngine;
 public class PlayerCircle : MonoBehaviour
 {
   public float JumpForce = 10f;
-  public Rigidbody2D ball;
+  public Rigidbody2D ball ;
+  public string ballColor = "Yellow"; 
+  public SpriteRenderer ballSpriteRenderer;
+	
+  public Color colorCyan;
+	public Color colorYellow;
+	public Color colorMagenta;
+	public Color colorPink;
+  
+    void Start()
+    {
+      ball.velocity = Vector2.up * JumpForce;
+      setRandomColorForBall();
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,5 +28,33 @@ public class PlayerCircle : MonoBehaviour
          {
             ball.velocity = Vector2.up * JumpForce;  
          }     
+    }
+
+
+ 
+    void setRandomColorForBall()
+    {
+       int index = Random.Range(0, 4); 
+
+       switch (index)
+       {
+        case 0:
+          ballColor = "Yellow";
+          ballSpriteRenderer.color =  colorYellow;
+          break;
+        case 1:
+          ballColor = "Green";
+          ballSpriteRenderer.color =  colorMagenta;
+          break;
+        case 2:
+          ballColor = "Blue";
+          ballSpriteRenderer.color = colorCyan;
+          break;
+        case 3:
+          ballColor = "Pink";
+          ballSpriteRenderer.color = colorPink;
+          break;
+
+       } 
     }
 }
